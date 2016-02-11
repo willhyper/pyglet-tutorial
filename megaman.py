1ee7dce63148e20801919b2a83361f7f1be481ec
willhyper <willhyper@gmail.com>
2016-02-10 20:25:39 -0800

    use bit operation, add UP and DOWN feature, fix megaman can go outside window

--- megaman.py
+++ megaman.py
@@ -1,23 +1,22 @@
 import pyglet
-import datetime
 import random
 from pyglet.window import key
 
 window = pyglet.window.Window(width=150, height=400)
 megaman = pyglet.resource.image('mega_man.jpg')
-megaman.anchor_x = megaman.width // 2
+megaman.anchor_x = megaman.width >> 1
 megaman.anchor_y = 0
 
 ball = pyglet.resource.image('ball.gif')
-ball.anchor_x = ball.width // 2
+ball.anchor_x = ball.width >> 1
 ball.anchor_y = ball.height
 
 
 class Game(object):
     def __init__(self):
-        self.megaman_x = window.width // 2
+        self.megaman_x = window.width >> 1
         self.megaman_y = 0
-        self.ball_x = window.width // 2
+        self.ball_x = window.width >> 1
         self.ball_y = window.height
         self.score = 0
         self.gameover = False
@@ -35,9 +34,13 @@ score_label = pyglet.text.Label(str(game.score),
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.RIGHT:
-        game.megaman_x += 10
+        game.megaman_x += 10 if game.megaman_x < window.width else 0
     elif symbol == key.LEFT:
-        game.megaman_x -= 10
+        game.megaman_x -= 10 if game.megaman_x >= 0 else 0
+    elif symbol == key.UP:
+        game.megaman_y += 10 if game.megaman_y < 100 else 0
+    elif symbol == key.DOWN:
+        game.megaman_y -= 10 if game.megaman_y >= 0 else 0
 
 
 @window.event
@@ -55,8 +58,7 @@ def on_draw():
 
 def ball_drop(dt):
     # if colision?
-    if abs(game.ball_x - game.megaman_x) < 30 and abs(game.ball_y -
-            game.megaman_y) < 30:
+    if abs(game.ball_x - game.megaman_x) < 30 and abs(game.ball_y - game.megaman_y) < 30:
         game.gameover = True
         return
 
