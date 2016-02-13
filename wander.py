9c0043fead2f0bb120605a8858cdaa64965dcd36
willhyper <willhyper@gmail.com>
2016-02-13 11:52:44 -0800

    refactor

--- wander.py
+++ wander.py
@@ -6,11 +6,13 @@ megaman = pyglet.resource.image('mega_man.jpg')
 megaman.anchor_x = megaman.width >> 1
 megaman.anchor_y = megaman.height >> 1
 
+
 def coroutine(func):
-    def start(*args,**kwargs):
-        cr = func(*args,**kwargs)
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
         cr.next()
         return cr
+
     return start
 
 
@@ -20,42 +22,55 @@ class GameState(object):
         self.y = window.height >> 1
         self.count = 0
 
-        self.tick=self.tick()
+        # instantiate coroutine
+        self.tick = self.tick()
+        self.walk = self.walk()
+
+    def send(self, arg):
+        self.tick.send(arg)
 
     @coroutine
     def tick(self):
+        while True:
+            symbol = yield
+            self.walk.send(symbol)
+
+    @coroutine
+    def walk(self):
         step = 20
         while True:
             symbol = (yield)
 
-            self.count += 1
-
             if symbol == key.RIGHT:
                 self.x += step if self.x < window.width else 0
+                self.count += 1
             elif symbol == key.LEFT:
                 self.x -= step if self.x >= 0 else 0
+                self.count += 1
             elif symbol == key.UP:
                 self.y += step if self.y < window.height else 0
+                self.count += 1
             elif symbol == key.DOWN:
                 self.y -= step if self.y >= 0 else 0
+                self.count += 1
 
-    def send(self,arg):
-        self.tick.send(arg)
 
 
 gs = GameState()
-score_label = pyglet.text.Label(str(0),x=0,y=0)
+walk_count = pyglet.text.Label(str(gs.count), x=0, y=0)
 
 
 @window.event
 def on_key_press(symbol, modifiers):
     gs.send(symbol)
 
+
 @window.event
 def on_draw():
     window.clear()
     megaman.blit(gs.x, gs.y)
-    score_label.text = str(gs.count)
-    score_label.draw()
+    walk_count.text = str(gs.count)
+    walk_count.draw()
+
 
 pyglet.app.run()
